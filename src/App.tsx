1c98833f174acb17a1665c35f4f770775f309c0c
Mark Schmid <mark.schmid94@gmail.com>
2024-01-28 20:38:47 -0500

    removed extra files from CRA, setup routes, added 3 quiz files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Quiz from "./routes/Quiz";
+import Home from "./routes/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+            <h1 className="appTitle">Get Quizzzed</h1>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route key="Home" path="/" element={<Home />} />
+                    <Route key="Quiz" path="quiz/:topic" element={<Quiz />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
-
-export default App;
